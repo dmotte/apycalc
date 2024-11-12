aa3e1d609d5000f1c9a56d1e6546975d1f730c34
dmotte <37443982+dmotte@users.noreply.github.com>
2024-11-12 23:15:03 +0100

    Completed fetch.sh script

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ cd example/
 python3 -mvenv venv
 venv/bin/python3 -mpip install -r requirements.txt
 
-# TODO fetch data
+# TODO write to customize path to the fetcher
+./fetch.sh ~/git/misc/python-scripts/ohlcv-fetchers/yahoo-finance.py '^GSPC' 2000-01-01T00Z > SPX500.csv
 python3 -mapycalc --help TODO | venv/bin/python3 plots.py -ra
 ```
--- example/fetch.sh
+++ example/fetch.sh
@@ -2,14 +2,17 @@
 
 set -e
 
-readonly fetcher= # TODO path to misc/python-scripts/ohlcv-fetchers/yahoo-finance.py (pass it as first arg)
+# Local path to the https://github.com/dmotte/misc/blob/main/python-scripts/ohlcv-fetchers/yahoo-finance.py script
+readonly fetcher=${1:?}
 
-symbol=${1:?}
+readonly symbol=${2:?} dt_start=${3:-1970-01-01T00Z}
 
 if [[ "$(uname)" = MINGW* ]]
     then py=$(dirname "$fetcher")/venv/Scripts/python
     else py=$(dirname "$fetcher")/venv/bin/python3
 fi
 
-# TODO customizable datetime as arg
-"$py" "$fetcher" "$symbol" -i1wk -d1970-01-01T00Z -f'{:.6f}' | tr -d '\r'
+# We separately invoke the fetcher script in advance, to avoid masking its
+# return value
+data=$("$py" "$fetcher" "$symbol" -i1wk -d"$dt_start" -f'{:.6f}')
+echo "$data" | tr -d '\r'
